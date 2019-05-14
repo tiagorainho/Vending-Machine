4d3acf69a9c225d97a96564129f8e086737c23b3
Tiago <tiago.rainho@ua.pt>
2019-05-14 12:22:39 +0100

    f

--- projetoFinal/states.vhd
+++ projetoFinal/states.vhd
@@ -15,6 +15,8 @@ architecture Behav of states is
 	signal PS, NS  : state;
 	signal s_reset : std_logic := '0';
 	signal s_switchesCount : unsigned(1 downto 0);
+	signal s_price : unsigned(50,3);
+	signal s_money : unsigned(100,3);
 begin
 
 sync_proc: process(clk)
@@ -42,15 +44,33 @@ begin
 		--s_switchesCount <= unsigned(switches(3 downto 3)) + unsigned(switches(2 downto 2)) + unsigned(switches(1 downto 1)) + unsigned(switches(0 downto 0),1);
 		--if (s_switchesCount = to_unsigned(1 , 1)) then
 		if(switches="1000" or switches="0100" or switches="0010" or switches="0001") then
-			NS <= S;
+		
+			with switches select
+				s_price <=  to_unsigned(30,3) when "0001",
+								to_unsigned(30,3) when "0010",
+								to_unsigned(50,3) when "0100",
+								to_unsigned(45,3) when others;
+			
+			if(keys(3)='1' or keys(2)='1' or keys(1)='1' or keys(0)='1') then
+				NS <= S;
+			end if;
+		elsif(switches="0000") then
+			NS <= I;
 		else
-			NS <= E;
+			NS <= SB;
 		end if;
 			
 	when E =>
+		if(switches="1000" or switches="0100" or switches="0010" or switches="0001") then
+			NS <= S;
+		elsif(switches="0000") then
+			NS <= I;
+		else
+			NS <= E;
+		end if;
 		
-	
 	when S =>
+		
 	
 	when F =>
 	
